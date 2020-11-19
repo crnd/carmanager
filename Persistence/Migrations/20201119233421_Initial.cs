107ac6e1bc08de49d824b42003550ef676bbb2d7
Henri Nieminen <henri@purkki.fi>
2020-11-20 00:54:03 +0100

    Add initial database migration

---
+++ Persistence/Migrations/20201119233421_Initial.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarManager.Persistence.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PublicId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Make = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Power = table.Column<int>(type: "int", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Updated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Deleted = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_Make_Model_Power_Deleted",
+                table: "Cars",
+                columns: new[] { "Make", "Model", "Power", "Deleted" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cars_PublicId",
+                table: "Cars",
+                column: "PublicId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
