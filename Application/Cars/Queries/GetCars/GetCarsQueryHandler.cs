3159a687cadb759dc4a337563b0d5380674f94d5
Henri Nieminen <henri@purkki.fi>
2020-12-11 01:41:01 +0100

    Only fetch needed columns when querying all cars
    Using Select(car => mapper.Map<CarDto>(car)) when converting a car
    entity to a CarDto will cause all columns to be fetched from the car
    table. As a lot of these columns will be discarded immediately when
    mapping to a CarDto, it is better to use ProjectTo when doing to
    conversion. ProjectTo will only fetch the columns that will be used in
    the CarDto.

--- Application/Cars/Queries/GetCars/GetCarsQueryHandler.cs
+++ Application/Cars/Queries/GetCars/GetCarsQueryHandler.cs
@@ -1,9 +1,9 @@
 ﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using CarManager.Persistence;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,18 +12,18 @@ namespace CarManager.Application.Cars.Queries.GetCars
 	public class GetCarsQueryHandler : IRequestHandler<GetCarsQuery, List<CarDto>>
 	{
 		private readonly CarManagerContext context;
-		private readonly IMapper mapper;
+		private readonly IConfigurationProvider provider;
 
-		public GetCarsQueryHandler(CarManagerContext context, IMapper mapper)
+		public GetCarsQueryHandler(CarManagerContext context, IConfigurationProvider provider)
 		{
 			this.context = context;
-			this.mapper = mapper;
+			this.provider = provider;
 		}
 
 		public async Task<List<CarDto>> Handle(GetCarsQuery request, CancellationToken cancellationToken)
 		{
 			return await context.Cars
-				.Select(c => mapper.Map<CarDto>(c))
+				.ProjectTo<CarDto>(provider)
 				.ToListAsync(cancellationToken);
 		}
 	}
